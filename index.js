497f91e489b3a7d71ecd16a38ca0a45931807e1e
Murugan Kannan <machaendran9221@gmail.com>
2021-07-25 14:33:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,19 +3,7 @@
  */
 
 // run `node index.js` in the terminal
-var server1 = require('./server-1');
-var server2 = require('./server-2');
 var loadBalancer = require('./load-balancer');
-
-// Starting the server 1 
-server1.listen(3000, function() {
-  console.log('The server1 is up and running now');
-});
-
-// Starting the server 2
-server2.listen(3001, function() {
-  console.log('The server2 is up and running now');
-});
 
 
 // Server details
