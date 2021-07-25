ee1315481817b67a8d46c1e666d65e31c4805f83
Murugan Kannan <machaendran9221@gmail.com>
2021-07-25 13:06:26 +0530

    added  comments

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
+/**
+ * @author Murugan Kannan (https://github.com/murugan-kannan)
+ */
+
 // run `node index.js` in the terminal
 var server1 = require('./server-1');
 var server2 = require('./server-2');
 var loadBalancer = require('./load-balancer');
 
+// Starting the server 1 
 server1.listen(3000, function() {
   console.log('The server1 is up and running now');
 });
 
+// Starting the server 2
 server2.listen(3001, function() {
   console.log('The server2 is up and running now');
 });
 
+
+// Server details
 var serverConfig = [
   {
     scheme: 'http',
@@ -22,6 +30,5 @@ var serverConfig = [
   }
 ];
 
-console.log(loadBalancer);
-
+// Starting the server with server config
 loadBalancer.start(3002, serverConfig);
