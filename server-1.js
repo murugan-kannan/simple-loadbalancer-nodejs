9c990ef3cc644d2e49986d860d6b60950f5fce36
Murugan Kannan <machaendran9221@gmail.com>
2021-07-25 13:04:15 +0530

    added hello endpoint

--- server-1.js
+++ server-1.js
@@ -1,7 +1,20 @@
+/**
+ * @author Murugan Kannan (https://github.com/murugan-kannan)
+ */
+
 var http = require('http');
 
+// To handle the request.
 var server = http.createServer(function(req, res) {
-  res.end('Server 1');
+  
+  if (req.url === "/") {
+    res.end('Data - Server 1');
+  } else if (req.url === "/hello") {
+    res.end('Hello - Server 1');
+  } else {
+    res.end();
+  }
+
 });
 
 module.exports = server;
--- server-2.js
+++ server-2.js
@@ -1,7 +1,20 @@
+/**
+ * @author Murugan Kannan (https://github.com/murugan-kannan)
+ */
+
 var http = require('http');
 
-var server = http.createServer(function(req, res) {
-  res.end('Server 2');
+// To handle the request.
+var server = http.createServer(function (req, res) {
+  
+  if (req.url === "/") {
+    res.end('Data - Server 2');
+  } else if (req.url === "/hello") {
+    res.end('Hello - Server 2');
+  } else {
+    res.end();
+  }
+  
 });
 
 module.exports = server;
